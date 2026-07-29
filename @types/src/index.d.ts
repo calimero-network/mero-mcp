e2fb5dc75bf5569a4ea53da1e47cd3f38a8bfc57
rtb-12 <ronit_c@me.iitr.ac.in>
2026-07-29 19:52:27 +0530

    chore: remove the express echo scaffold and scaffold the stdio server

--- @types/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-// Type definitions for src
-// Project: https://github.com/calimero-network/mero-mcp
-// Definitions by: XI
-
-declare module 'src' {
-  // Export any necessary types or interfaces that might be used
-  // when importing from 'src'
-} 
